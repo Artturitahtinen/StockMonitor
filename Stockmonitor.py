a50a1c9b9a800cf2fd5a4fd76d9be9a5a478d4ec
Artturitahtinen <tahtinen.artturi@gmail.com>
2020-08-16 12:32:17 +0300

    Added comments for kauppalehti stock urls

--- Stockmonitor.py
+++ Stockmonitor.py
@@ -2,6 +2,11 @@ from Services.RequestParseService import RequestParseService
 from Webdriver import Webdriver
 
 fortumUrl = 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/FORTUM'
+#KoneCranes OYJ
+#Nordea Bank ABP
+#Orion B
+#Outokumpu
+#Sampo A
 
 
 driver = Webdriver()
