4035033b04af85f48d3b3267761f4b673e1ce9de
Artturitahtinen <tahtinen.artturi@gmail.com>
2020-08-18 13:04:26 +0300

    Refactoring

--- Services/Mail_service.py
+++ Services/Mail_service.py
@@ -1,10 +1,25 @@
 import smtplib, ssl
 
+
 class Mail_service:
 
-    port = 465
-    password = 
-
-    def __init__(self, stock_dict){
+    def __init__(self, stock_dict, sender_email, password, recipient_email, smtp_server, port):
         self.stock_dict = stock_dict
-    }
+        self.sender_email = sender_email
+        self.password = password
+        self.recipient_email = recipient_email
+        self.smtp_server = smtp_server
+        self.port = port
+
+    def send_email(self):
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(self.smtp_server, self.port, context=context) as server:
+            server.login(self.sender_email, self.password)
+            message = """
+            Subject: Stock(s) has risen significantly
+
+            {stock_name} has risen over 50% from purchase price.
+            Current price: {stock_price} €"""
+
+            server.sendmail(self.sender_email, self.recipient_email, message.format(stock_name=stock_name, stock_price=stock_price))
--- Stockmonitor.py
+++ Stockmonitor.py
@@ -1,17 +1,58 @@
 from Services.Request_parse_service import Request_parse_service
 from Webdriver.Webdriver import Webdriver
 from Services.Mail_service import Mail_service
+import numpy as np
+import configparser
+config= configparser.ConfigParser()
+config.read(r'D:/Python projects/StockMonitor/Configs/config.ini')
 
+sender_email = config['MAIL']['sender_email']
+password = config['MAIL']['password']
+recipient_email = config['MAIL']['recipient_email']
+smtp_server = config['MAIL']['smtp_server']
+port = config['MAIL']['port']
 
 stock_urls = {
-    'fortumUrl': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/FORTUM',
-    'koneCranesUrl': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/KCR',
-    'nordeaBank': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/NDA%20FI',
-    'outokumpu': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/OUT1V',
-    'sampoA': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/SAMPO'
+    'orionB_url': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/ORNBV',
+    'konecranes_url': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/KCR',
+    'nordeaBank_url': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/NDA%20FI',
+    'outokumpu_url': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/OUT1V',
+    'sampoA_url': 'https://www.kauppalehti.fi/porssi/porssikurssit/osake/SAMPO'
 }
-companies_sell_labels = ['Fortum sell price','Konecranes sell price','Nordea Bank sell price','Outokumpu','Sampo A']
+companies_sell_labels = ['Orion B sell price','Konecranes sell price','Nordea Bank sell price','Outokumpu','Sampo A']
 sell_prices = []
+increase = [1.40, 1.50, 1.50, 1.50, 1.50]
+target_prices = [55.58, 30.33, 10.22, 3.315, 38.955]
+
+stocks = {
+    'Orion B': {
+        'sell_prices': 0.0,
+        'increase': 0.40,
+        'target_price': 55.58
+    },
+    'Konecranes': {
+        'sell_prices': 0.0,
+        'increase': 0.50,
+        'target_price': 30.33
+    },
+    'Nordea Bank': {
+        'sell_prices': 0.0,
+        'increase': 0.50,
+        'target_price': 10.22
+    },
+    'Outokumpu': {
+        'sell_prices': 0.0,
+        'increase': 0.50,
+        'target_price': 3.315
+    },
+    'Sampo A': {
+        'sell_prices': 0.0,
+        'increase': 0.50,
+        'target_price': 38.955
+    }    
+}
+
+
 
 def main():
     #Initialize chromedriver
@@ -24,17 +65,16 @@ def main():
         sell_price = rps.parse_request()
         sell_prices.append(sell_price)
 
-    #Dictionary of company names and its sell price values
-    stock_dict = make_dict()
+    sell_prices_to_dict(sell_prices, stocks)
 
-    mail_service = Mail_service(stock_dict)
-    mail_service.send_mail()
+    #if(check_mail_send_conditions(sell_prices, target_prices)):
+    #   mail_service = Mail_service(stock_dict, sender_email, recipient_email, smtp_server, port)
+    #   mail_service.send_email()
     
-    
-
-def make_dict():
-    companies_sell_prices = dict(zip(companies_sell_labels, sell_prices))
-    return companies_sell_prices
+def sell_prices_to_dict(sell_prices, stocks):
+    iter_sell_prices_list = iter(sell_prices)
+    for key, value in stocks.items():
+        print(key, value)
 
 if __name__ == "__main__":
     main()
